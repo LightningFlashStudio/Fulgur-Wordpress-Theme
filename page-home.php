f3fad360d8f090c766bf19df5cded06d8b107915
Kornell Gai Rancy III <lightningflashstudio@gmail.com>
2017-08-04 08:40:51 -0400

    Last Edit's

--- page-home.php
+++ page-home.php
@@ -42,6 +42,7 @@
 				$lastBlog = new WP_Query('type=post&posts_per_page=1');
 
 				if( $lastBlog->have_posts() ):
+
 					while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
 
 						<?php get_template_part('/parts/template-parts/content/content', get_post_format()); ?>
